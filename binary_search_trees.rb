9e716b3e08cf351ebc5f4a9361cd4e91e8d4d7fe
Alan Kwong <akwong910@gmail.com>
2023-06-11 16:57:03 -0400

    remove comments from binary_search_trees.rb

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -175,16 +175,3 @@ class Tree
         return @root = build_tree(inorder)
     end
 end
-
-# a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-# a.insert(7000)
-# a.insert(300)
-# a.insert(500)
-# a.insert(900)
-# a.pretty_print
-# p a.balanced?
-# p a.rebalance
-# p a.pretty_print
-# p a.balanced?
-
-# p a.level_order
